743c4bf8936e494fea78942b2b744c118a5e9c02
biqontie <awesome.abaildaev@yandex.kz>
2022-04-10 17:18:36 +0600

    first

--- binary-search/binary_search.py
+++ binary-search/binary_search.py
@@ -1,11 +1,23 @@
-from tkinter import W
 
 
 def binary_search(lst, to_find):
-    # search for the element to_find inside lst
-    if to_find in lst:
-        return lst.index(to_find)
+    # search for the element to_find inside lsti
+    first = 0
+    last = len(lst)-1
+    index = -1
+    val = to_find
+    while (first <= last) and (index == -1):
+        mid = (first+last)//2
+        if lst[mid] == val:
+            index = mid
+        else:
+            if val < lst[mid]:
+                last = mid - 1
+            else:
+                first = mid + 1
+    if val == lst[index]:
+        return index
     else:
-        return lst.index(to_find-1)
+        return -1
     # if found, return index of element
     # else return -1
