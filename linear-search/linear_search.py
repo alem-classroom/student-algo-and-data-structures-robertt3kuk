393aa985c3a6163e1ba2e43524e6346f7408db14
biqontie <awesome.abaildaev@yandex.kz>
2022-04-10 18:57:54 +0600

    first

--- linear-search/linear_search.py
+++ linear-search/linear_search.py
@@ -1,4 +1,11 @@
 def linear_search(lst, to_find):
   # search for the element to_find inside lst
+  for i in range(0,len(lst-1)):
+      if lst[i] == to_find:
+          return i 
+  return -1
+    
+
+                
   # if found, return index of element
   # else return -1
