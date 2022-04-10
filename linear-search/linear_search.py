94c9fb24384f90bfffddc7a2bc0d2a0796607938
biqontie <awesome.abaildaev@yandex.kz>
2022-04-10 19:15:36 +0600

    sorted 4real

--- linear-search/linear_search.py
+++ linear-search/linear_search.py
@@ -1,6 +1,6 @@
 def linear_search(lst, to_find):
   # search for the element to_find inside lst
-  lst = lst.sorted()
+  lst.sort()
   for i in range(0,len(lst)):
       if lst[i] == to_find:
           return i 
